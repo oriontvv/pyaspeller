8134a94e9ca8bc4c1d4ff1b1ca4d805424d88b2d
oriontvv <taranov.vv@gmail.com>
2015-08-30 21:46:41 +0300

    construct args for speller class

--- pyaspeller/__init__.py
+++ pyaspeller/__init__.py
@@ -24,6 +24,9 @@ def create_args_parser():
 
     parser.add_argument('text_or_path_or_url', nargs='?', default='')
 
+    parser.add_argument('--config_path', default=None,
+                        help="Configuration file path")
+
     parser.add_argument('--verbose', action="store_const",
                         help="increase verbosity level",
                         dest="log_level", const=logging.INFO,
@@ -47,25 +50,74 @@ def create_args_parser():
     parser.add_argument('-d', '--dictionary', default=None,
                         help="path to custom json dictionary file")
 
-    parser.add_argument('-r', '--report', default='console',
+    parser.add_argument('-r', '--report_type', default='console',
                         choices=('console', 'html', 'markdown', 'json'),
                         help="type of report")
 
-    parser.add_argument('--check-yo', default=False, action='store_true',
-                        help="Check the correctness of using the " +
+    parser.add_argument('--check_yo', default=False, action='store_true',
+                        help="check the correctness of using the " +
                              "letter “Ё” (Yo) in Russian texts")
 
-    parser.add_argument('--ignore_uppercase', action='store_true',
-                        help="ignore words written in capital letters")
+    parser.add_argument('--find-repeat-words', action='store_true',
+                        help="highlight repetitions of words, consecutive. " +
+                             "For example, I flew to to to Cyprus")
+
+    parser.add_argument('--flag-latin', action='store_true',
+                        help="celebrate words, written in Latin, as erroneous")
+
+    parser.add_argument('--ignore-tags', action='store_true',
+                        help="ignore HTML tags. "
+                             "Default: code,kbd,object,samp,script,style,var "
+                             "Option to formats html and markdown")
+
+    parser.add_argument('--ignore_capitalization', action='store_true',
+                        help="ignore the incorrect use of UPPERCASE/lowercase "
+                             "letters, for example, in the word moscow")
 
     parser.add_argument('--ignore_digits', action='store_true',
                         help="ignore words with numbers, such as avp17h4534")
 
-    parser.add_argument('--ignore_url', action='store_true',
+    parser.add_argument('--ignore-latin', action='store_true',
+                        help="ignore words, written in Latin, like 'madrid'")
+
+    parser.add_argument('--ignore-roman-numerals', action='store_true',
+                        help="ignore Roman numerals I, II, III, ...")
+
+    parser.add_argument('--ignore_uppercase', action='store_true',
+                        help="ignore words written in capital letters")
+
+    parser.add_argument('--ignore_urls', action='store_true',
                         help="ignore Internet addresses, email "
                              "addresses and filenames")
 
+    parser.add_argument('--max-requests', default=2,
+                        help="Max count of requests at a time")
+
     return parser
+
+
+def create_speller(args):
+    speller = YandexSpeller(format=args.format,
+                            lang=args.lang,
+                            report_type=args.report_type,
+                            is_debug=args.log_level == logging.DEBUG,
+                            check_yo=args.check_yo,
+                            ignore_urls=args.ignore_urls,
+                            ignore_tags=args.ignore_tags,
+                            ignore_capitalization=args.ignore_capitalization,
+                            ignore_digits=args.ignore_digits,
+                            ignore_latin=args.ignore_latin,
+                            ignore_roman_numerals=args.ignore_roman_numerals,
+                            ignore_uppercase=args.ignore_uppercase,
+                            find_repeat_words=args.find_repeat_words,
+                            flag_latin=args.flag_latin)
+    if args.config_path:
+        speller.config_path = args.config_path
+
+    if args.dictionary:
+        speller.dictionary = args.dictionary
+
+    return speller
 
 
 def main():
@@ -74,7 +126,8 @@ def main():
     args = create_args_parser().parse_args()
     logging.basicConfig(level=args.log_level)
 
-    speller = YandexSpeller(args)
+    speller = create_speller(args)
+
     result = speller.spell(args.text_or_path_or_url)
     for generator in result:
         for item in generator:
--- pyaspeller/speller.py
+++ pyaspeller/speller.py
@@ -68,16 +68,216 @@ class Speller(object):
 
 class YandexSpeller(Speller):
 
-    def __init__(self, options):
-        self._options = options
-        self._query_template = 'http://speller.yandex.net/services/spellservice.json/' + \
-                               'checkText?text={text}&lang={lang}'
+    def __init__(self, format='auto', lang=None, config_path=None,
+                 dictionary=None, report_type=None, max_requests=2,
+                 is_debug=False, check_yo=False, ignore_urls=False,
+                 ignore_tags=False, ignore_capitalization=False,
+                 ignore_digits=False, ignore_latin=False,
+                 ignore_roman_numerals=False, ignore_uppercase=False,
+                 find_repeat_words=True, flag_latin=True):
+
+        self._format = format
+        self._lang = lang or ['en', 'ru']
+        if not isinstance(self._lang, (list, tuple)):
+            self._lang = [self._lang]
+        self._config_path = config_path or ''
+        self._dictionary = dictionary or {}
+        self._report_type = report_type or 'console'
+
+        self._check_yo = check_yo
+        self._ignore_urls = ignore_urls
+        self._ignore_tags = ignore_tags
+        self._ignore_capitalization = ignore_capitalization
+        self._ignore_digits = ignore_digits
+        self._ignore_latin = ignore_latin
+        self._ignore_roman_numerals = ignore_roman_numerals
+        self._ignore_uppercase = ignore_uppercase
+        self._find_repeat_words = find_repeat_words
+        self._flag_latin = flag_latin
+
+        self._max_requests = max_requests
+        self._is_debug = is_debug
+
+        self._api_query = 'http://speller.yandex.net/services/spellservice.json/' + \
+                          'checkText?text={text}&lang={lang}&options={options}'
+
+    @property
+    def format(self):
+        """Get format"""
+        return self._format
+
+    @format.setter
+    def format(self, value):
+        """Set format"""
+        self._format = value
+
+    @property
+    def lang(self):
+        """Get lang"""
+        return self._lang
+
+    @lang.setter
+    def lang(self, value):
+        """Set lang"""
+        self._lang = value
+
+    @property
+    def config_path(self):
+        """Get config_path"""
+        return self._config_path
+
+    @config_path.setter
+    def config_path(self, value):
+        """Set config_path"""
+        self._config_path = value
+
+    @property
+    def dictionary(self):
+        """Get dictionary"""
+        return self._dictionary
+
+    @dictionary.setter
+    def dictionary(self, value):
+        """Set dictionary"""
+        self._dictionary = value
+
+    @property
+    def report_type(self):
+        """Get report_type"""
+        return self._report_type
+
+    @report_type.setter
+    def report_type(self, value):
+        """Set report_type"""
+        self._report_type = value
+
+    @property
+    def check_yo(self):
+        """Get check_yo"""
+        return self._check_yo
+
+    @check_yo.setter
+    def check_yo(self, value):
+        """Set check_yo"""
+        self._check_yo = value
+
+    @property
+    def ignore_urls(self):
+        """Get ignore_urls"""
+        return self._ignore_urls
+
+    @ignore_urls.setter
+    def ignore_urls(self, value):
+        """Set ignore_urls"""
+        self._ignore_urls = value
+
+    @property
+    def ignore_tags(self):
+        """Get ignore_tags"""
+        return self._ignore_tags
+
+    @ignore_tags.setter
+    def ignore_tags(self, value):
+        """Set ignore_tags"""
+        self._ignore_tags = value
+
+    @property
+    def ignore_capitalization(self):
+        """Get ignore_capitalization"""
+        return self._ignore_capitalization
+
+    @ignore_capitalization.setter
+    def ignore_capitalization(self, value):
+        """Set ignore_capitalization"""
+        self._ignore_capitalization = value
+
+    @property
+    def ignore_digits(self):
+        """Get ignore_digits"""
+        return self._ignore_digits
+
+    @ignore_digits.setter
+    def ignore_digits(self, value):
+        """Set ignore_digits"""
+        self._ignore_digits = value
+
+    @property
+    def ignore_latin(self):
+        """Get ignore_latin"""
+        return self._ignore_latin
+
+    @ignore_latin.setter
+    def ignore_latin(self, value):
+        """Set ignore_latin"""
+        self._ignore_latin = value
+
+    @property
+    def ignore_roman_numerals(self):
+        """Get ignore_roman_numerals"""
+        return self._ignore_roman_numerals
+
+    @ignore_roman_numerals.setter
+    def ignore_roman_numerals(self, value):
+        """Set ignore_roman_numerals"""
+        self._ignore_roman_numerals = value
+
+    @property
+    def ignore_uppercase(self):
+        """Get ignore_uppercase"""
+        return self._ignore_uppercase
+
+    @ignore_uppercase.setter
+    def ignore_uppercase(self, value):
+        """Set ignore_uppercase"""
+        self._ignore_uppercase = value
+
+    @property
+    def find_repeat_words(self):
+        """Get find_repeat_words"""
+        return self._find_repeat_words
+
+    @find_repeat_words.setter
+    def find_repeat_words(self, value):
+        """Set find_repeat_words"""
+        self._find_repeat_words = value
+
+    @property
+    def flag_latin(self):
+        """Get flag_latin"""
+        return self._flag_latin
+
+    @flag_latin.setter
+    def flag_latin(self, value):
+        """Set flag_latin"""
+        self._flag_latin = value
+
+    @property
+    def max_requests(self):
+        """Get max_requests"""
+        return self._max_requests
+
+    @max_requests.setter
+    def max_requests(self, value):
+        """Set max_requests"""
+        self._max_requests = value
+
+    @property
+    def is_debug(self):
+        """Get is_debug"""
+        return self._is_debug
+
+    @is_debug.setter
+    def is_debug(self, value):
+        """Set is_debug"""
+        self._is_debug = value
 
     def _spell_text(self, text):
 
         words = '+'.join(re.findall(r'\w+', text))
-        lang = ','.join(self._options.lang)
-        query = self._query_template.format(text=quote(words), lang=lang)
+        print (self._lang)
+        lang = ','.join(self._lang)
+        query = self._api_query.format(text=quote(words), lang=lang,
+                                       options=self.api_options)
         logging.debug("query: " + query)
 
         responce = read_url(query).decode('utf-8')
@@ -88,3 +288,22 @@ class YandexSpeller(Speller):
         responce = json.loads(responce)
         for item in responce:
             yield item
+
+    @property
+    def api_options(self):
+        options = 0
+        if self._ignore_uppercase:
+            options += 1
+        if self._ignore_digits:
+            options += 2
+        if self._ignore_urls:
+            options += 4
+        if self._find_repeat_words:
+            options += 8
+        if self._ignore_latin:
+            options += 16
+        if self._flag_latin:
+            options += 128
+        if self._ignore_capitalization:
+            options += 512
+        return options
