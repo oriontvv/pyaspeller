2c5985c118269fa7019d39a323faf752c1cf376f
oriontvv <taranov.vv@gmail.com>
2023-07-12 00:23:57 +0300

    add future-annotations

--- tests/test_speller.py
+++ tests/test_speller.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import pytest
 import requests_mock
 
--- tests/test_word.py
+++ tests/test_word.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import pytest
 from pyaspeller.errors import BadArgumentError
 
